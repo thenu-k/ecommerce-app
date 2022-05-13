bfe934e2567e6d9fc14d9e81508664dcdf7aa421
Thenu Kaluarachchi <thenujaya.k@gmail.com>
2022-05-13 16:31:16 +0530

    Created a basic header.

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
 Making emmet work inside js file------------------
     Inside settings.json -> includelanguages -> Added 'javascript': 'html'
+
+Making CSS autocomplete word inside styled-components---------------
+    ctr+p => ext install styled-components.vscode-styled-components
--- views/src/Layout/Layout.js
+++ views/src/Layout/Layout.js
@@ -1,15 +1,43 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
+import './Main.css'
 
 const Layout = () => {
+
   return (
     <>
       <header>
-        <nav></nav>
+        <nav class="center">
+          <div class="header top outer" aria-label="Header Top">
+            <div class="logo outer header" aria-label='Logo'>
+              <h1><a href="/">Shop</a></h1>
+            </div>
+            <div class="header search_bar outer" aria-label='Search Bar'>
+              <form><input type="text" placeholder='Search Items'/></form>
+            </div>
+            <div class="header action_list outer" aria-label='User Actions'>
+              <button class="account_icon outer" aria-label="Account Button">Account</button>
+              <button class="cart_icon outer" aria-label="Cart Button">Cart</button>
+            </div>
+          </div>
+          <div class="header bottom outer" aria-label="Header Bottom">
+            <ul class="header categories" aria-label='Category List'>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/new_arrivals">New Arrivals</Link></li>
+              <li><Link to="/sale">Sale</Link></li>
+              <li><Link to="/popular">Popular</Link></li>
+              <li><Link to="/account">Account</Link></li>
+            </ul>
+            <div class="header bottom misc_text" aria-label="Misc Text">
+              <p>All our products are build from eco-friendly resources</p>
+            </div>
+          </div>
+        </nav>
       </header>
       <Outlet/>
     </>
   )
 }
+
 
 export default Layout
