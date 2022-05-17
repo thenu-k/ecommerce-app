21774bc2e76caa8e9aa13e6d15db223482f023b3
Thenu Kaluarachchi <thenujaya.k@gmail.com>
2022-05-17 19:05:30 +0530

    Made search bar responsive

--- views/src/Pages/Search/SearchBar/SearchBar.js
+++ views/src/Pages/Search/SearchBar/SearchBar.js
@@ -10,16 +10,6 @@ const SearchBar = (props) => {
         <h3 className='search_query'>{props.search_query}</h3>
         <form  >
             <input type="text" name="Search" className="searchbar input"></input>
-            <select className="filter sort_by" aria-label='Drop Down Box: Sort By'>
-                <option value="Popularity">Popularity</option>
-                <option value="Price: Ascending" >Price: Ascending</option>
-                <option value="Price: Descending">Price: Descending</option>
-            </select>
-            <select name="" className="filter brand" >
-                <option value="Jike">Jike</option>
-                <option value="Jadidas">Jadidas</option>
-                <option value="Jouie Juitton">Jouie Juitton</option>
-            </select>
         </form>
     </section>
     </>
