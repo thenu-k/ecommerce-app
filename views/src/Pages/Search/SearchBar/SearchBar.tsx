ef9b65022507dbb3cd4b7544f6865f6259b7ad0b
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-20 00:19:34 +0530

    Added search bar functionality

--- views/src/Pages/Search/SearchBar/SearchBar.tsx
+++ views/src/Pages/Search/SearchBar/SearchBar.tsx
@@ -1,14 +1,26 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import './SearchBar.css'
+
+//React router
+import {useNavigate} from 'react-router-dom'
 
 const SearchBar = (props:any) => {
   const [filters, setFilters] = useState({sort_by: null, brand: null})
+
+  //Link changes
+  const navigate = useNavigate()
+
+  //Search Item function
+  const searchItem = (e: React.FormEvent):void => {
+    e.preventDefault();
+    navigate('/search/new', {replace: true})
+  }
 
   return (
     <>
     <section className='searchbar outer container' aria-label='Search Bar Container'>
         <h3 className='search_query'>{props.search_query}</h3>
-        <form  >
+        <form  onSubmit={(e)=>searchItem(e)}>
             <input type="text" name="Search" className="searchbar input"></input>
         </form>
     </section>
