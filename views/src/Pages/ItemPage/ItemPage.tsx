f896dbde1613529dde4fe1c223c88f17fe283b28
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-20 09:38:11 +0530

    Created item page skeleton logic

--- views/src/Pages/ItemPage/ItemPage.tsx
+++ views/src/Pages/ItemPage/ItemPage.tsx
@@ -1,30 +1,49 @@
 import React, {FC, useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
+import LoadingIcon from '../../Models/LoadingIcon/LoadingIcon'
+import './ItemPage.css'
 
+interface IDetails {
+  wtf: string
+}
+type Details = IDetails | undefined
+
+
+//Function
 const ItemPage: FC = () => {
   const params = useParams()
 
   //States
   const [loading, setLoading] = useState(true)
+  const [details, setDetails] = useState<Details>()
   const [itemID, setItemID] = useState(params.id)
 
   //Fetching data
   useEffect(()=>{
-    itemID
-      ? getItemData(itemID)
+    itemID                      //Because item id might not exist
+      ? getItemData(itemID)      
       : console.log()     //Fix!
-  })
+  }, [])
 
   //Get and set data function
   const getItemData = (itemID:string):void => {
     const URL = `http://localhost/item/details/${itemID}`
     fetch(URL).then(res=>{return res.json()})
-    .then(data=> console.log(data))
+    .then(data=> {
+      setDetails(data)
+      setLoading(false)
+    })
   }
 
   return (
-    <>Yo
-    </>
+    <div id='ItemPage' className={loading.toString()}>
+      {
+        loading 
+            ? <LoadingIcon/>
+            : <LoadingIcon/>
+            
+      }
+    </div>
   )
 }
 
