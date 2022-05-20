caf61cf611ef39ef710949e785177fc73bb8c101
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-20 10:51:17 +0530

    Minor changes

--- views/src/App.tsx
+++ views/src/App.tsx
@@ -1,35 +1,43 @@
-import {useState} from 'react'
-import {BrowserRouter, Routes, Route, Outlet, Navigate, Link} from 'react-router-dom'
+import { useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+  Link,
+} from "react-router-dom";
 
 //React Components
-import Layout from './Layout/Layout'; import Auth from './Auth/Auth'
-import Home from './Pages/Home/Home' 
-import Account from './Pages/Account/Account'
-import Error404 from './Pages/404/Error404'
-import Search from './Pages/Search/Search'
-import ItemPage from './Pages/ItemPage/ItemPage';
+import Layout from "./Layout/Layout";
+import Auth from "./Auth/Auth";
+import Home from "./Pages/Home/Home";
+import Account from "./Pages/Account/Account";
+import Error404 from "./Pages/404/Error404";
+import Search from "./Pages/Search/Search";
+import ItemPage from "./Pages/ItemPage/ItemPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Layout />}>
           {/* Unprotected Routes */}
-          <Route index element={<Home/>}/>
-          <Route path="search/:search_query" element={<Search/>}/>
-          <Route path="item/:id" element={<ItemPage/>}/>
-          
+          <Route index element={<Home />} />
+          <Route path="search/:search_query" element={<Search />} />
+          <Route path="item/:id" element={<ItemPage />} />
+
           {/* Protected Routes */}
-          <Route element={<Auth/>}>
-            <Route path="account" element={<Account/>}/>
+          <Route element={<Auth />}>
+            <Route path="account" element={<Account />} />
           </Route>
 
           {/* 404 Page */}
-          <Route path="*" element={<Error404/>}/>
+          <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
--- views/src/index.tsx
+++ views/src/index.tsx
@@ -1,19 +1,19 @@
-import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import LoadingScreen from './Models/LoadingScreen/LoadingScreen';
-import store from './StateManager/store'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import LoadingScreen from "./Models/LoadingScreen/LoadingScreen";
+import store from "./StateManager/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);  //Added any type
+const root = ReactDOM.createRoot(document.getElementById("root")!); //Added any type
 
-const App = React.lazy(()=>import('./App'))
+const App = React.lazy(() => import("./App"));
 
 root.render(
-    <>
-        <Suspense fallback={<LoadingScreen/>}>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </Suspense>
-    </>
+  <>
+    <Suspense fallback={<LoadingScreen />}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Suspense>
+  </>
 );
