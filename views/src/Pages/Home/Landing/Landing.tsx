a6f77aa4625a24e616a6329bbaac1857f1be9158
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-21 23:48:49 +0530

    Revamoed landing section

--- views/src/Pages/Home/Landing/Landing.tsx
+++ views/src/Pages/Home/Landing/Landing.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Landing.css'
-const landing_image = require('./../../../Assets/landing.jpg')
+const landing_image = require('./../../../Assets/shoesLanding.jpg')
 
 const Landing = () => {
 
   return (
     <>
-    <section aria-label='Landing Section' className="landing container outer">
-        <div className="landing container inner">
-            <div className="landing image outer center" aria-label='Landing Image'>
-                <img src={landing_image} alt="Lady in casual wear" loading='eager'/>
-                <div className="landing text outer center" aria-label='Landing Text'>
-                   <h2>Summer Sale</h2>
-                 </div>
-            </div>
+    <section aria-label='Landing Section' className="landing container outer center">
+        <img src={landing_image} alt="Lady in casual wear" loading='eager'/>
+        <div className="landing text outer center" aria-label='Landing Text'>
+            <h2>Bounce a Bit</h2>
+            <Link to='' className='center'>Shop</Link>
         </div>
     </section>
     </>
