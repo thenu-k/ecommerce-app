2ad9760ac0310ae2d82cd8ccd6fe967700b7700e
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-23 14:34:09 +0530

    Touched up css

--- views/src/Pages/Home/Featured/Featured.tsx
+++ views/src/Pages/Home/Featured/Featured.tsx
@@ -18,6 +18,34 @@ const Featured = () => {
               <div className="price">$100</div>
             </div>
           </div>
+          <div className="featured_wrap">
+            <div className="featured_image center"><img src='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8be0676348c04300a579ae14013777d5_9366/Cloudfoam_Pure_2.0_shoes_White_GY4485_01_standard.jpg' alt="" /></div>
+            <div className='center featured_text'>
+              <div className="title">Athletic Shoes</div>
+              <div className="price">$100</div>
+            </div>
+          </div>
+          <div className="featured_wrap">
+            <div className="featured_image center"><img src='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8be0676348c04300a579ae14013777d5_9366/Cloudfoam_Pure_2.0_shoes_White_GY4485_01_standard.jpg' alt="" /></div>
+            <div className='center featured_text'>
+              <div className="title">Athletic Shoes</div>
+              <div className="price">$100</div>
+            </div>
+          </div>
+          <div className="featured_wrap">
+            <div className="featured_image center"><img src='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8be0676348c04300a579ae14013777d5_9366/Cloudfoam_Pure_2.0_shoes_White_GY4485_01_standard.jpg' alt="" /></div>
+            <div className='center featured_text'>
+              <div className="title">Athletic Shoes</div>
+              <div className="price">$100</div>
+            </div>
+          </div>
+          <div className="featured_wrap">
+            <div className="featured_image center"><img src='https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8be0676348c04300a579ae14013777d5_9366/Cloudfoam_Pure_2.0_shoes_White_GY4485_01_standard.jpg' alt="" /></div>
+            <div className='center featured_text'>
+              <div className="title">Athletic Shoes</div>
+              <div className="price">$100</div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
