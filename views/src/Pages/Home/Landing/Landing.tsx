1e2cb26225493e97b3420cb7f0438070868d0a83
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-23 12:34:50 +0530

    Added min max values to margin and changed slogan

--- views/src/Pages/Home/Landing/Landing.tsx
+++ views/src/Pages/Home/Landing/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = () => {
     <section aria-label='Landing Section' className="landing container outer center">
         <img src={landing_image} alt="Lady in casual wear" loading='eager'/>
         <div className="landing text outer center" aria-label='Landing Text'>
-            <h2>Bounce a Bit.</h2>
+            <h2>Walk Ahead</h2>
             <Link to='' className='center'>Shop</Link>
         </div>
     </section>
