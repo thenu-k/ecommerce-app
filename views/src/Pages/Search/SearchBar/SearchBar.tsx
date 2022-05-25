d5704540554cc5fc293677f692f99ca8c0b796c5
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-05-25 20:56:39 +0530

    Removed position fixed from the search bar

--- views/src/Pages/Search/SearchBar/SearchBar.tsx
+++ views/src/Pages/Search/SearchBar/SearchBar.tsx
@@ -19,9 +19,9 @@ const SearchBar = (props:any) => {
   return (
     <>
     <section className='searchbar outer container' aria-label='Search Bar Container'>
-        <h3 className='search_query'>{props.search_query}</h3>
+        {/* <h3 className='search_query'>{props.search_query}</h3> */}
         <form  onSubmit={(e)=>searchItem(e)}>
-            <input type="text" name="Search" className="searchbar input"></input>
+            <input type="text" name="Search" className="searchbar input" placeholder='Search Items'></input>
         </form>
     </section>
     </>
